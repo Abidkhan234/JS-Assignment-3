b23d3a98752f4e39c8b2b39a2150b83f996ba225
Abidkhan234 <abidshahkhan237@gmail.com>
2024-10-28 02:32:21 +0500

    chapter 31 to 24 done

--- app.js
+++ app.js
@@ -100,7 +100,7 @@
 
 // document.write("String:",word);
 
-// document.write("<br>"); 
+// document.write("<br>");
 
 // var updatedWord = word.replace(delWord,replaceWord);
 
@@ -135,7 +135,7 @@
 //     if (message.slice(i, i + delWord.length) === delWord) {
 //         updatedArr += replace;
 
-//         i += delWord.length - 1; 
+//         i += delWord.length - 1;
 //     } else {
 //         updatedArr += arr[i];
 //     }
@@ -438,8 +438,6 @@
 
 //? Question no 7 start
 
-
-
 //? Question no 7 end
 
 //? Question no 8 start
@@ -460,4 +458,243 @@
 
 //? Question no 8 end
 
-//! Chapter no 21 to 25 end
+//! Chapter no 26 to 30 end
+
+//! Chapter no 31 to 34 start
+
+//? Question no 1 start
+
+// var currentTime = new Date();
+
+// document.write(CurrentTime);
+
+//? Question no 1 end
+
+//? Question no 2 start
+
+// var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+// var currentMonth = new Date().getMonth();
+
+// alert("Current month: " + monthNames[currentMonth]);
+
+//? Question no 2 end
+
+//? Question no 3 start
+
+// var dayNames = ["Sun","Mon","Tues","Wed","Thur","Fri","Sat"];
+
+// var currentDay = new Date().getDay();
+
+// alert("Today is: " + dayNames[currentDay]);
+
+//? Question no 3 end
+
+//? Question no 4 start
+
+// var currentDay = new Date().getDay();
+
+// var dayNames = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+
+// var flag = false;
+
+// for (var i = 0; i < dayNames.length; i++) {
+//     dayNames[i] = dayNames[i].toLowerCase();
+//     if (dayNames[currentDay] == "sat" || dayNames[currentDay] == "sun") {
+//         flag = true;
+//     }
+// }
+
+// if (flag) {
+//     alert("It`s Fun day")
+// } else {
+//     alert("It`s Work day")
+// }
+
+//? Question no 4 end
+
+//? Question no 5 start
+
+// var currentDate = new Date().getDate();
+
+// console.log(currentDate)
+
+// if(currentDate < 16){
+//     alert("First fifteen days of the month");
+// }else{
+//     alert("Last fifteen days of the month");
+// }
+
+//? Question no 5 end
+
+//? Question no 6 start
+
+// var currentDate = new Date();
+
+// document.write("Current Date: ", currentDate)
+
+// document.write("<br>");
+
+// var currentDateInMilliSec = currentDate.getTime();
+
+// document.write("Elapsed milliseconds since January 1,1970: ", currentDateInMilliSec);
+
+// document.write("<br>");
+
+// var currentDateInMin = currentDate.getTime();
+
+// document.write("Elapsed milliseconds since January 1,1970: ", Math.round(currentDateInMin / (1000 * 60)));
+
+//? Question no 6 end
+
+//? Question no 7 start
+
+// var currentHours = new Date().getHours();
+
+// if (currentHours < 12) {
+//     alert("It`s AM");
+// } else {
+//     alert("It`s PM");
+// }
+
+//? Question no 7 end
+
+//? Question no 8 start
+
+// var laterDate = new Date("December 31 , 2020");
+
+// document.write("Later Date: ",laterDate);
+
+//? Question no 8 end
+
+//? Question no 9 start
+
+// var ramadanDate = new Date("2024, March 12");
+
+// var ramadanDateInMilliSec = ramadanDate.getTime();
+
+// var currentDate = new Date();
+
+// var currentDateInMilliSec = currentDate.getTime();
+
+// var dayPassed = currentDateInMilliSec - ramadanDateInMilliSec;
+
+// document.write(Math.round(dayPassed / (1000 * 60 * 60 * 24)), " days have passed since 1st Ramadan,2024");
+
+//? Question no 9 end
+
+//? Question no 10 start
+
+// var referenceDate = new Date("Dec 05 , 2023");
+
+// var referenceDateInMilliSec = referenceDate.getTime();
+
+// var currentDate = new Date();
+
+// var currentDateInMilliSec = currentDate.getTime();
+
+// var secPassed = currentDateInMilliSec - referenceDateInMilliSec;
+
+// document.write("On reference date " + referenceDate + " , " + Math.round(secPassed / (1000)) + " seconds had passed since beginning of 2023");
+
+//? Question no 10 end
+
+//? Question no 11 start
+
+// var currentDate = new Date();
+
+// document.write("current date: ",currentDate)
+
+// var currentDateInHours = currentDate.getHours();
+
+// currentDate.setHours(currentDateInHours - 1);
+
+// document.write("<br>");
+
+// document.write("1 hours ago, it was ",currentDate );
+
+//? Question no 11 end
+
+//? Question no 12 start
+
+// var currentDate = new Date();
+
+// alert("Current date: " + currentDate);
+
+// currentDate.setFullYear(currentDate.getFullYear() - 100);
+
+// alert("100 year back, it was " + currentDate);
+
+//? Question no 12 end
+
+//? Question no 13 start
+
+// var userAge = Number(prompt("Enter your age"));
+
+// document.write("Your age is: ",userAge);
+
+// document.write("<br>");
+
+// var currentDate = new Date();
+
+// currentDate.setFullYear(currentDate.getFullYear() - userAge);
+
+// document.write("Your birth year is: ",currentDate.getFullYear());
+
+//? Question no 13 end
+
+//? Question no 14 start
+
+// document.write("<h1>K-Electric Bill</h1>")
+
+// var userName = prompt("Enter your name");
+
+// document.write("Customer Name: ", `<b> ${userName} </b>`);
+
+// var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+// var currentMonth = new Date().getMonth();
+
+// document.write("<br>");
+// document.write("<br>");
+
+// document.write("Month: ", `<b> ${monthNames[currentMonth]} </b>`);
+
+// document.write("<br>");
+// document.write("<br>");
+
+// var NumOfUnits = Number(prompt("Enter how many units you consume"));
+
+// document.write("Number of units: ", `<b> ${NumOfUnits} </b>`);
+
+// document.write("<br>");
+// document.write("<br>");
+
+// var unitCharge = 45;
+
+// document.write("Charges per unit: ", `<b> ${unitCharge} </b>`)
+
+// document.write("<br>");
+// document.write("<br>");
+
+// var amountPayableDueDate = (NumOfUnits * unitCharge).toFixed(2);
+
+// document.write("Amount Payable (within Due Date): ", `<b> ${amountPayableDueDate} </b>`);
+
+// document.write("<br>");
+// document.write("<br>");
+
+// var lateSurcharge = 350;
+
+// document.write("Late payment surcharge: ", `<b> ${lateSurcharge} </b>`);
+
+// document.write("<br>");
+// document.write("<br>");
+
+// var amountPayableAfterDueDate = (parseFloat(amountPayableDueDate) + lateSurcharge).toFixed(2);
+
+// document.write("Gross Amount Payable (after Due Date): ", `<b> ${amountPayableAfterDueDate} </b>`);
+
+//? Question no 14 end
+
+//! Chapter no 31 to 34 end
